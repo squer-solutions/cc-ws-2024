315ba2bd8b6aff69746681765bad07e0a64c0285
mrtnsch <schwarzm1995@gmail.com>
2024-04-05 07:49:35 +0200

    initial commit

---
+++ data/postgres_bootstrap.sql
@@ -0,0 +1,15 @@
+--TODO: Create proper migration script for workshop
+-- Create the orders table
+create table orders (
+                        order_id serial primary key,
+                        customer_id int,
+                        total float,
+                        created_at timestamp default now()
+);
+
+-- Populate it with a few values
+insert into orders(customer_id, total) values (1,50);
+insert into orders(customer_id, total) values (2,100);
+insert into orders(customer_id, total) values (2,50);
+insert into orders(customer_id, total) values (3,10);
+insert into orders(customer_id, total) values (4,90);
---
+++ src/main/java/io/squer/ccworkshop2024/CcWorkshop2024Application.java
@@ -0,0 +1,13 @@
+package io.squer.ccworkshop2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcWorkshop2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcWorkshop2024Application.class, args);
+    }
+
+}

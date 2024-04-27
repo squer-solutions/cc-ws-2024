ba5faa6346596b9921fdace46d092d43e66c3b21
mrtnsch <schwarzm1995@gmail.com>
2024-04-27 10:59:32 +0200

    refactor shell exercise 6

--- exercises/exercise6/java/transformer/src/main/java/io/squer/services/TransformerService.java
+++ exercises/exercise6/java/transformer/src/main/java/io/squer/services/TransformerService.java
@@ -1,32 +1,24 @@
 package io.squer.services;
 
-import Transformer.Models.Address;
-import Transformer.Models.Customer;
-import cdc.public$.customers.Envelope;
-import cdc.public$.customers.Key;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
-
 import java.util.Properties;
-import java.util.UUID;
 
 public class TransformerService {
-
+    static final String STREAMS_APPLICATION_ID = "transformer-development-app";
+    static final String BOOTSTRAP_SERVERS_CONFIG = "http://localhost:9092";
+    static final String SCHEMA_REGISTRY_URL_CONFIG = "http://localhost:8081";
+    static final String CLIENT_ID_CONFIG = "transformer-development";
     private final Properties properties;
     static final String CDC_TOPIC = "cdc.public.customers";
     static final String TRANSFORMER_TOPIC = "customer-transformed-topic";
 
-    public TransformerService(Properties props) {
-        properties = props;
+    public TransformerService() {
+        properties = getStreamsConfig();
     }
 
     public void run() {
@@ -54,5 +46,17 @@ public class TransformerService {
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
         streams.start();
+    }
+
+    private static Properties getStreamsConfig() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, STREAMS_APPLICATION_ID);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, CLIENT_ID_CONFIG);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, true);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL_CONFIG);
+        return props;
     }
 }

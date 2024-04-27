61d26ad831da90d3633abc3138f88246598bd685
mrtnsch <schwarzm1995@gmail.com>
2024-04-27 11:12:26 +0200

    refactor exercise 5

--- exercises/exercise5/java/streaming-app-finished/src/main/java/io/squer/services/StreamingAppWorker.java
+++ exercises/exercise5/java/streaming-app-finished/src/main/java/io/squer/services/StreamingAppWorker.java
@@ -3,6 +3,7 @@ package io.squer.services;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
@@ -10,16 +11,18 @@ import org.apache.kafka.streams.kstream.Produced;
 import java.util.Properties;
 
 public class StreamingAppWorker {
-
+    private static final String STREAMS_APPLICATION_ID = "my-first-streams";
+    private static final String BOOTSTRAP_SERVERS_CONFIG = "localhost:9092";
+    private static final String CLIENT_ID_CONFIG = "streams-client-example";
+    private static final String INPUT_TOPIC = "src-topic";
+    private static final String OUTPUT_TOPIC = "dest-topic";
     private final Properties properties;
-    static final String INPUT_TOPIC = "src-topic";
-    static final String OUTPUT_TOPIC = "dest-topic";
 
-    public StreamingAppWorker(Properties props){
-        properties = props;
+    public StreamingAppWorker() {
+        properties = getStreamsConfig();
     }
 
-    public void Run(){
+    public void run(){
         final StreamsBuilder builder = new StreamsBuilder();
 
         KStream<String, String> sourceStream = builder.stream(INPUT_TOPIC);
@@ -37,6 +40,16 @@ public class StreamingAppWorker {
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
         streams.start();
+    }
+
+    private static Properties getStreamsConfig() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, STREAMS_APPLICATION_ID);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, CLIENT_ID_CONFIG);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
+        return props;
     }
 
 }
--- exercises/exercise5/java/streaming-app/src/main/java/io/squer/services/StreamingAppWorker.java
+++ exercises/exercise5/java/streaming-app/src/main/java/io/squer/services/StreamingAppWorker.java
@@ -3,12 +3,12 @@ package io.squer.services;
 import java.util.Properties;
 
 public class StreamingAppWorker {
-    static final String STREAMS_APPLICATION_ID = "my-first-streams";
-    static final String BOOTSTRAP_SERVERS_CONFIG = "localhost:9092";
-    static final String CLIENT_ID_CONFIG = "streams-squerify-client-example";
+    private static final String STREAMS_APPLICATION_ID = "my-first-streams";
+    private static final String BOOTSTRAP_SERVERS_CONFIG = "localhost:9092";
+    private static final String CLIENT_ID_CONFIG = "streams-client-example";
+    private static final String INPUT_TOPIC = "src-topic";
+    private static final String OUTPUT_TOPIC = "dest-topic";
     private final Properties properties;
-    static final String INPUT_TOPIC = "src-topic";
-    static final String OUTPUT_TOPIC = "dest-topic";
 
     public StreamingAppWorker() {
         properties = getStreamsConfig();
